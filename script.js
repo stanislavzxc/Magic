a2aa1b30a5715ff4c5e7266fde470d6d8b81ddcc
stas <102482885+stanislavzxc@users.noreply.github.com>
2024-03-23 11:38:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let link = document.getElementById('myLink');
+
+let checkbox = document.getElementById('check_box'); 
+
+
+let input1 = document.getElementsByName('number1')[0]
+let input2 = document.getElementsByName('number2')[0]
+let input3 = document.getElementsByName('number3')[0]
+let input4 = document.getElementsByName('number4')[0]
+let input5 = document.getElementsByName('number5')[0]
+let input6 = document.getElementsByName('number6')[0]
+let input7 = document.getElementsByName('number7')[0]
+function myButton() {
+  if (input1.value === '' || input2.value === ''|| input3.value === '' || input4.value === '' || input5.value === '' || input6.value === '' || input7.value === '') {
+    alert("Заполните все поля");
+  } else if (checkbox.checked === false) {
+    alert('нажмите на галочку');
+  } else {
+    link.href = 'pay.html';
+  }
+}
---
+++ vars.js
@@ -0,0 +1,7 @@
+let input1 = document.getElementsByName('number1')[0]   // кто интересен 
+let input2 = document.getElementsByName('number2')[0]   // положение
+let input3 = document.getElementsByName('number3')[0]   // знак задиака
+let input4 = document.getElementsByName('number4')[0]   // имя
+let input5 = document.getElementsByName('number5')[0]   // фамилия
+let input6 = document.getElementsByName('number6')[0]   // дата рождения
+let input7 = document.getElementsByName('number7')[0]   // gmail
